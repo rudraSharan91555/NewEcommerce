ae889f7681c2828be2b285daad38ced52956f2a6
Rudra Sharan <rudrasharan45@gmail.com>
2025-02-15 10:49:27 +0530

    Home Page Api

--- app/Http/Controllers/Front/HomePageController.php
+++ app/Http/Controllers/Front/HomePageController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Front;
 
 use App\Http\Controllers\Controller;
+use App\Models\Brand;
+use App\Models\Category;
 use App\Models\HomeBanner;
 use App\Traits\ApiResponse;
 use Illuminate\Http\Request;
@@ -13,6 +15,8 @@ class HomePageController extends Controller
     public function  getHomeData(){
         $data = [];
         $data['banner'] = HomeBanner::get();
+        $data['categories'] = Category::with('products')->get();
+        $data['brands'] = Brand::get();
         return $this->success(['data'=>$data],'Sucessfully data fetched');
     }
 }
